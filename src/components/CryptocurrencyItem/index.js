388eec9c8a70884cfe475f0fb5355e6352f6728d
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-05-25 10:23:24 +0530

    Update index.js

--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -1,20 +1,20 @@
 // Write your JS code here
-import './index.css'
+import "./index.css";
 
-const CryptocurrencyItem = props => {
-  const {eachCurrency} = props
-  const {currencyLogo, currencyName, euroValue, id, usdValue} = eachCurrency
+const CryptocurrencyItem = (props) => {
+  const { eachCurrency } = props;
+  const { currencyLogo, currencyName, euroValue, id, usdValue } = eachCurrency;
   return (
-    <li>
-      <div>
-        <img src={currencyLogo} alt={currencyName} />
-        <p>{currencyName}</p>
+    <li className="list-item-bg">
+      <div className="crptyo-currency-bg">
+        <img src={currencyLogo} alt={currencyName} className="crptyo-image" />
+        <p className="crptyo-name">{currencyName}</p>
       </div>
-      <div>
-        <p>{usdValue}</p>
-        <p>{euroValue}</p>
+      <div className="crypto-values">
+        <p className="crptyo-usd-value">{usdValue}</p>
+        <p className="crptyo-euro-value">{euroValue}</p>
       </div>
     </li>
-  )
-}
-export default CryptocurrencyItem
+  );
+};
+export default CryptocurrencyItem;
