ec85cd34f6eb95e8102a79db1d40155756469e47
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-06-29 10:18:05 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Home = () => {
+  const [msg, setMsg] = useState("");
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("http://localhost:3000", {
+        msg,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <form action="POST">
+      <textarea
+        name="text"
+        onChange={(e) => {
+          setMsg(e.target.value)
+        }}
+        placeholder="enter the text"
+      />
+      <br />
+      <input type="submit" onClick={submitForm} value="Submit"/>
+    </form>
+  );
+};
+export default Home;
